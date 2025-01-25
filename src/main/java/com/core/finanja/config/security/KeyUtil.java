b4fb4be1a513447a5b31cf29bb73989de2bdca98
John Aguirre <johnzcode@gmail.com>
2025-01-24 22:40:20 -0500

    agrego validacion para leer archivo key cuando es local y leer base64 cuando la app esta en render con el valor en la variable de entorno

--- src/main/java/com/core/finanja/config/security/KeyUtil.java
+++ src/main/java/com/core/finanja/config/security/KeyUtil.java
@@ -1,7 +1,7 @@
 package com.core.finanja.config.security;
 
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.security.KeyFactory;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
@@ -12,34 +12,40 @@ import java.util.Base64;
 public class KeyUtil {
 
     public static RSAPrivateKey getPrivateKey(String privateKeyPath) throws Exception {
-        try (FileInputStream fis = new FileInputStream(privateKeyPath)) {
-            byte[] keyBytes = fis.readAllBytes();
-            String privateKeyContent = new String(keyBytes);
-            privateKeyContent = privateKeyContent
-                    .replaceAll("\\s+", "");
+        byte[] decodedKey;
 
-            byte[] decodedKey = Base64.getDecoder().decode(privateKeyContent);
-            PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(decodedKey);
-            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-            return (RSAPrivateKey) keyFactory.generatePrivate(spec);
-        } catch (IOException e) {
-            throw new Exception("Error reading the private key file", e);
+        if (new File(privateKeyPath).exists()) {
+            try (FileInputStream fis = new FileInputStream(privateKeyPath)) {
+                byte[] keyBytes = fis.readAllBytes();
+                String privateKeyContent = new String(keyBytes).replaceAll("\\s+", "");
+                decodedKey = Base64.getDecoder().decode(privateKeyContent);
+            }
+        } else {
+            String privateKeyContent = privateKeyPath.replaceAll("\\s+", "");
+            decodedKey = Base64.getDecoder().decode(privateKeyContent);
         }
+
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(decodedKey);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return (RSAPrivateKey) keyFactory.generatePrivate(spec);
     }
 
     public static RSAPublicKey getPublicKey(String publicKeyPath) throws Exception {
-        try (FileInputStream fis = new FileInputStream(publicKeyPath)) {
-            byte[] keyBytes = fis.readAllBytes();
-            String publicKeyContent = new String(keyBytes);
-            publicKeyContent = publicKeyContent
-                    .replaceAll("\\s+", "");
+        byte[] decodedKey;
 
-            byte[] decodedKey = Base64.getDecoder().decode(publicKeyContent);
-            X509EncodedKeySpec spec = new X509EncodedKeySpec(decodedKey);
-            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-            return (RSAPublicKey) keyFactory.generatePublic(spec);
-        } catch (IOException e) {
-            throw new Exception("Error reading the public key file", e);
+        if (new File(publicKeyPath).exists()) {
+            try (FileInputStream fis = new FileInputStream(publicKeyPath)) {
+                byte[] keyBytes = fis.readAllBytes();
+                String publicKeyContent = new String(keyBytes).replaceAll("\\s+", "");
+                decodedKey = Base64.getDecoder().decode(publicKeyContent);
+            }
+        } else {
+            String publicKeyContent = publicKeyPath.replaceAll("\\s+", "");
+            decodedKey = Base64.getDecoder().decode(publicKeyContent);
         }
+
+        X509EncodedKeySpec spec = new X509EncodedKeySpec(decodedKey);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return (RSAPublicKey) keyFactory.generatePublic(spec);
     }
 }
