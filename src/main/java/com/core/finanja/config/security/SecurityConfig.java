54581398520e91b15a60af6611b622e5553cbe29
John Aguirre <johnzcode@gmail.com>
2025-01-25 22:33:59 -0500

    muevo clase a paquete correspondiente

--- src/main/java/com/core/finanja/config/security/SecurityConfig.java
+++ src/main/java/com/core/finanja/config/security/SecurityConfig.java
@@ -1,6 +1,5 @@
-package com.core.finanja.config;
+package com.core.finanja.config.security;
 
-import com.core.finanja.config.security.KeyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
