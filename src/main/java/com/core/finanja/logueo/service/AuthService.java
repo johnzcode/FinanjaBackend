6ce3e61f70b0a08cd927945c6a3b1af63c5c67a7
John Aguirre <johnzcode@gmail.com>
2025-01-26 22:43:13 -0500

    cambio orden de los servicios

--- src/main/java/com/core/finanja/logueo/service/AuthService.java
+++ src/main/java/com/core/finanja/logueo/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.core.finanja.logueo.service;
 
 import com.core.finanja.logueo.model.AuthResponse;
+import com.core.finanja.logueo.model.DTO.RecoverPwdRequest;
 import com.core.finanja.logueo.model.data.Usuario;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AccessTokenResponse;
 
@@ -9,9 +10,11 @@ import java.util.UUID;
 
 public interface AuthService {
 
+    Optional<OAuth2AccessTokenResponse> login(String emailOrUsername, String password);
+
     AuthResponse register(Usuario usuario) throws Exception;
 
-    Optional<OAuth2AccessTokenResponse> login(String emailOrUsername, String password);
+    AuthResponse sendReciverEmail(String user, String email) throws Exception;
 
     Optional<Usuario> findById(UUID id);
 }
